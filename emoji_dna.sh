7826028386209b7df800c6af4e15007cbc3a5cc4
Katie Saund <katiephd@umich.edu>
2020-12-30 18:10:50 -0500

    Keep only animal emoji. Add dashes for bonds. Add major and minor groove.

--- emoji_dna.sh
+++ emoji_dna.sh
@@ -1,4 +1,5 @@
-emojis=(😀😁😂🤣😃😄😅😆😉😊😋😎😍😘😗😙😚😐😑😶🙄😏😣😥😮🤐😯😪😫😴😌😛😜😝🤤😒😓😔😕🙃🤑😲😞😟😤😢😭😦😧😨😬😰😱😳😵😡😠😷🤒🤕🤢🤧😇🤠🤡🤥🤓😈👿👹👺💀👻👽🤖💩😺😸😹😻😼😽🙀😿😾👐🙌👏🤝👍👎👊🤛🤜🤞🤘👌👈👉👆👇🖐🖖👋🙏💍💄💋👄👅👂👃👣👁🗣👤👥👐🙌👏🤝👍👎👊✊🤛🤜🤘👌👈👉👆👇🤚🖐🖖👋🙏💍💄💋👄👅👂👃👣👁👀🗣👤👥🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐽🐸🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐚🐞🐜🦗🕷🕸🦂🐢🐍🦎🐙🦑🦐🦀🐡🐠🐟🐬🐳🐋🦈🐊🐅🐆🦍🐘🐪🐫🐃🐂🐄🐎🐖🐏🐑🐐🦌🐕🐩🐈🐓🦃🕊🐇🐁🐀🐿🦔🐾🐉🐲🌵🎄🌲🌳🌴🌱🌿️🍀🎍🎋🍃🍂🍁🍄🌾💐🌷🌹🥀🌺🌸🌼🌻🌞🌝🌛🌜🌚🌕🌖🌗🌘🌑🌒🌓🌔🌙🌎🌍🌏💫☄💥🔥🌪🌈🌤⛅🌊🌫🍏🍎🍐🍊🍋🍌🍉🍇🍓🍈🍒🍑🍍🥝🍅🍆🥑🥒🌶🌽🥕🥔🍠🥐🍞🥖🧀🥚🍳🥞🥓🍗🍖🌭🍔🍟🍕🥙🌮🌯🥗🥘🍝🍜🍲🍛🍣🍱🍤🍙🍚🍘🍥🍢🍡🍧🍨🍦🍰🎂🍮🍭🍬🍫🍿🍩🍪🌰🥜🍯🥛🍼🍶🍺🍻🥂🍷🥃🍸🍹🍾🥄🍴🍽🏐🏉🎾🎱🏓🏸🥅🏒🏑🏏🏹🎣🏂🏋🚕🚙🚌🚎🏎🚓🚑🚒🚐🚚🚛🚜🛴🚲🛵🏍🚨🚔🚍🚘🚖🚡🚠🚟🚃🚋🚞🚝🚄🚅🚈🚂🚆🚇🛬🛩💺🛰🚥🚏🗺🗿🗽🗼🏰🏯🏟🎡🏭🏢🏬🏣🏤🏥🏦🏨🏪🏫🏩💒🕍🕋⛩🛤🛣🗾🎑🏞🌅🌄🌠🎇🎆🌇🌆🏙🌃🚏🗺🗿🗽🗼🏰🏯🏟🎡🏝🏜🏡🏘🏚🏗🏭🏢🏬🏣🏤🏥🏦🏨🏪🏫🏩💒🕋⛩🛤🛣🗾🎑🏞🌅🌄🌠🎇🎆🌇🌆🏙💾💿📀📼📷📸📹🎥📽📟📠📺📻🎙🎚🎛⏱⏲⏰🕰🔋🔌💡🔦🕯🗑🛢💸💵💴💶💷💰🔨🛠⛏🔩🔫💣🔪🗡📿💈🔬🕳💊💉🌡🚽🚰🚿🛁🛀🛀🛀🛀🛀🛀🛌🖼🧳🛒🎁🎈🎏🎀🎊🎉🎎🏮🎐📩📨📧💌📥📤📦🏷📪📫📬📭📮📯📜📃📄📑📊📈📉🗒🗓📆📅📇📂🗂🗞📰📓📔📒📕📗📘📙📚📖🔖🔗📎🖇📐📏📌📍🔎🔏🔐🔒🔓💛💚💙💜🖤💔💓💗💖💘💝💟📛🚫🚷🕤🕥🚩🏳)
+emojis=(🙉🙊🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐚🐞🐜🦗🕷🕸🦂🐢🐍🦎🐙🦑🦐🦀🐡🐠🐟🐬🐳🐋🦈🐊🐅🐆🦍🐘🐪🐫🐃🐂🐄🐎🐖🐏🐑🐐🦌🐕🐩🐈🐓🦃🕊🐇🐁🐀🐿)
+
 
 num_emoji=${#emojis}
 num_emoji="$(($num_emoji - 1))"
@@ -6,26 +7,26 @@ left_index=$((1 + RANDOM % num_emoji))
 right_index=$((1 + RANDOM % num_emoji))
 
 # select emoji by find a substring of length one at the offset given (index)
-left_emoji=${emojis:left_index:1}
-right_emoji=${emojis:right_index:1}
+le=${emojis:left_index:1}
+re=${emojis:right_index:1}
 
-echo "$left_emoji   $right_emoji"
-echo " $left_emoji $right_emoji"
-echo "  $left_emoji"
-echo " $right_emoji $left_emoji"
-echo "$right_emoji   $left_emoji"
-echo "$right_emoji   $left_emoji"
-echo " $right_emoji $left_emoji"
-echo "  $right_emoji"
-echo " $left_emoji $right_emoji"
-echo "$left_emoji   $right_emoji"
-echo "$left_emoji   $right_emoji"
-echo " $left_emoji $right_emoji"
-echo "  $left_emoji"
-echo " $right_emoji $left_emoji"
-echo "$right_emoji   $left_emoji"
-echo "$right_emoji   $left_emoji"
-echo " $right_emoji $left_emoji"
-echo "  $right_emoji"
-echo " $left_emoji $right_emoji"
-echo "$left_emoji   $right_emoji"
+echo " $le---$re"
+echo "    $le-$re"
+echo "      $re"
+echo "    $re-$le"
+echo "  $re---$le"
+echo "$re---$le"
+echo " $re-$le"
+echo "  $le"
+echo " $le-$re"
+echo "  $le---$re"
+echo "   $le---$re"
+echo "     $le-$re"
+echo "      $re"
+echo "    $re-$le"
+echo "  $re---$le"
+echo "$re---$le"
+echo " $re-$le"
+echo "  $le"
+echo " $le-$re"
+echo "  $le---$re"
